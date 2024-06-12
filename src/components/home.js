de704919d2f75dee536373fd5f50a217cbc89c82
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-06-12 12:15:07 +0530

    did typewriter updation

--- src/components/home.js
+++ src/components/home.js
@@ -1,23 +1,41 @@
-import "../style/home.css"
-import blob from "../assets/blob4.gif"
-import progammer from "../assets/transparent-programmer-laptop-man-armchair-work-man-in-armchair-using-laptop-for-workentertainmen661931a65fa392.77797011.png"
+import "../style/home.css";
+import blob from "../assets/blob4.gif";
+import progammer from "../assets/transparent-programmer-laptop-man-armchair-work-man-in-armchair-using-laptop-for-workentertainmen661931a65fa392.77797011.png";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+
 const Home = () => {
     const email = "sandeepsreekumar4067@gmail.com";
+    const [text] = useTypewriter({
+        words: ["Front-End Developer", "Back-End Developer"],
+        loop: false
+    });
 
     const handleMailButtonClick = () => {
         window.location.href = `mailto:${email}`;
     };
-    return ( 
+
+    // Split the text to apply custom color to "Developer"
+    const splitText = text.split(' ');
+    const firstPart = splitText.slice(0, -1).join(' ');
+    const lastWord = splitText[splitText.length - 1];
+
+    return (
         <div className="home-component" id="home">
             <div className="home-info-container">
                 <div className="name-and-details">
                     <h1>Hey There</h1>
-                    <h2>I'm <span>Sandeep Sreekumar</span></h2>
-                    <h3>I'm a <span>Front-End</span> Developer</h3>
+                    <h2>I'm <span className="firstpart">Sandeep Sreekumar</span></h2>
+                    <div className="wrapper">
+                        <h3>
+                            I'm a <span className="firstpart">{firstPart}</span> <span className={lastWord === 'Developer' ? 'white-developer' : ''}>{lastWord}</span>
+                        </h3>
+                        <Cursor cursorColor="white" />
+                    </div>
                 </div>
                 <div className="download-section">
                     <input className="mail" type="button" value="Mail" onClick={handleMailButtonClick} />
-                    <a href="/CV_2024050712340861.pdf" download className="resume">Resume</a>                </div>
+                    <a href="/CV_2024050712340861.pdf" download className="resume">Resume</a>
+                </div>
             </div>
             <div className="blob-and-programmer">
                 <div className="blob">
@@ -28,7 +46,7 @@ const Home = () => {
                 </div>
             </div>
         </div>
-     );
-}
- 
+    );
+};
+
 export default Home;
